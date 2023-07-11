5a368aa7e07eb0d6466f2fb13c5f8327b4d2c0e4
Danial Raza <danialrazafb@gmail.com>
2023-07-11 07:40:03 +0200

    feat: add quote router to express app

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import { config } from "dotenv"
 import express from "express"
 import cors from "cors"
 
+import { quoteRouter } from "./quote/quote.router"
+
 config()
 
 if (!process.env.PORT) process.exit(1)
@@ -12,5 +14,7 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+
+app.use("/api/quotes", quoteRouter)
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`))

2dd23a8f02b80f20d095d88a5359f59d3f390ba0
Danial Raza <danialrazafb@gmail.com>
2023-07-11 07:17:28 +0200

    feat: add quote router file

---
+++ backend/src/quote/quote.router.ts
@@ -0,0 +1,27 @@
+import { Router, type Request, type Response } from "express"
+
+import { getQuote, listQuotes } from "./quote.service"
+import type { Quote } from "../types"
+
+export const quoteRouter = Router()
+
+quoteRouter.get("/", async (request: Request, response: Response) => {
+  try {
+    const quotes = await listQuotes()
+    return response.status(200).json(quotes)
+  } catch (error) {
+    return response.status(500).json(error.message)
+  }
+})
+
+quoteRouter.get("/:id", async (request: Request, response: Response) => {
+  const id: Quote["id"] = parseInt(request.params.id, 10)
+
+  try {
+    const quote = await getQuote(id)
+    if (quote) return response.status(200).json(quote)
+    else return response.status(404).json({ message: "Quote not found" })
+  } catch (error) {
+    return response.status(500).json(error.message)
+  }
+})

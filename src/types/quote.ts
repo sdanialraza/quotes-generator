497c090aed4a53697a19b25a09b2c3ae3458074c
Danial Raza <danialrazafb@gmail.com>
2023-07-29 14:26:05 +0200

    feat: introduce raw quotes
    As the data is static right now, we can just store them in a file, but an API will be used in the future.

---
+++ src/types/quote.ts
@@ -0,0 +1,24 @@
+type Category =
+  | "Art"
+  | "Fashion"
+  | "Funny"
+  | "Inspirational"
+  | "Leadership"
+  | "Life"
+  | "Love"
+  | "Motivational"
+  | "Music"
+  | "People"
+  | "Sports"
+  | "Success"
+  | "Wisdom"
+  | "Work"
+
+export default interface Quote {
+  author?: string
+  category: Category[]
+  id: number
+  text: string
+  submitter: string
+  verified: boolean
+}

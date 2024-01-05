eecba4252337ff18fdd58adb5f9e56b03c89eab1
Danial Raza <danialrazafb@gmail.com>
2024-01-05 18:03:50 +0100

    style: design changes (#17)

--- src/pages/QuotesShow.tsx
+++ src/pages/QuotesShow.tsx
@@ -1,3 +1,4 @@
+import { FaGithub } from "react-icons/fa"
 import { useParams } from "react-router-dom"
 
 import { REPOSITORY_LINK } from "../util"
@@ -11,11 +12,10 @@ export default function QuotesShow() {
 
   return (
     <div className="flex h-screen flex-col items-center justify-center gap-2">
-      <Quote quote={quote} onCopy={handleCopy} onShare={handleShare} />
-      <button className="button bg-card-color hover:bg-gray-800" onClick={handleGenerate}>
-        Generate another quote
-      </button>
-      <ExternalLink href={REPOSITORY_LINK}>Source Code</ExternalLink>
+      <Quote quote={quote} onCopy={handleCopy} onGenerate={handleGenerate} onShare={handleShare} />
+      <ExternalLink href={REPOSITORY_LINK}>
+        <FaGithub size={24} />
+      </ExternalLink>
     </div>
   )
 }

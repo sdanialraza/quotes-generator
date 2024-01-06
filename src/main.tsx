2bfbf070a31f8d5d3e106e042715bc817b452a77
Danial Raza <danialrazafb@gmail.com>
2024-01-06 11:43:38 +0100

    chore: back to vercel

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { Analytics } from "@vercel/analytics/react"
 import { createRoot } from "react-dom/client"
 import { Toaster } from "react-hot-toast"
 import { StrictMode } from "react"
-import { createHashRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
 import "./styles/globals.css"
 import { routes } from "./pages"
@@ -11,6 +11,6 @@ createRoot(document.querySelector("#root")!).render(
   <StrictMode>
     <Analytics />
     <Toaster />
-    <RouterProvider router={createHashRouter(routes)} />
+    <RouterProvider router={createBrowserRouter(routes)} />
   </StrictMode>,
 )
